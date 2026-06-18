1cb7c0b4f28ee5db01dd945111ae23760c072bd5
candefondini <candefondini24@gmail.com>
2026-06-18 16:11:14 -0300

    subo archivos

---
+++ js/hero-slider.js
@@ -0,0 +1,50 @@
+const slides = document.querySelectorAll('.slide');
+const nextBtn = document.querySelector('.next');
+const prevBtn = document.querySelector('.prev');
+
+let current = 0;
+
+function showSlide(index){
+
+    slides.forEach(slide=>{
+        slide.classList.remove('active');
+    });
+
+    slides[index].classList.add('active');
+}
+
+nextBtn.addEventListener('click',()=>{
+
+    current++;
+
+    if(current >= slides.length){
+        current = 0;
+    }
+
+    showSlide(current);
+
+});
+
+prevBtn.addEventListener('click',()=>{
+
+    current--;
+
+    if(current < 0){
+        current = slides.length - 1;
+    }
+
+    showSlide(current);
+
+});
+
+setInterval(()=>{
+
+    current++;
+
+    if(current >= slides.length){
+        current = 0;
+    }
+
+    showSlide(current);
+
+},6000);
